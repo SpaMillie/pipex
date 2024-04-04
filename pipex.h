297519517ad62c3cdd52d16eb35b5270efe0f591
Milica  Spasic <militsaspasic@gmail.com>
2024-04-04 16:52:27 +0300

    started handlling arguments

---
+++ pipex.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/04 16:00:31 by mspasic           #+#    #+#             */
+/*   Updated: 2024/04/04 16:07:04 by mspasic          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+
+//main.c
+
+//pipex.c
+
+#endif
